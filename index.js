c5d4efdebb463691c6865e0c6110b9ac08ffc917
samael <samael.65535@gmail.com>
2015-06-14 13:06:15 +0800

    Implement Super

--- index.js
+++ index.js
@@ -5,21 +5,24 @@
 module.exports = function (child, parent) {
     var fn = child.initialize || (function () {
         });
+    fn.__super__ = Object;
+    if (parent) {
+        fn.__super__ = parent;
+        //fn.prototype.constructor.prototype = parent.prototype;
+    }
+    fn.prototype.constructor = fn;
+
     for (var key in child) {
         if (key != "initialize") {
             fn.prototype[key] = child[key];
         }
     }
-    fn.__super__ = Object;
-    if (parent) {
-        fn.prototype.constructor.prototype = parent.prototype;
-        fn.__super__ = parent;
-    }
-    fn.prototype.constructor = fn;
+
     fn.prototype.super = function() {
-
-    }
-
+        var methodName = arguments[0];
+        var args = [].slice.call(arguments, 1);
+        return fn.__super__.prototype[methodName].apply(this, args);
+    };
     return fn
 };
 
