1535143b10dabc22cc8182961104ae7b868fb9b3
samael <samael.65535@gmail.com>
2015-06-14 13:15:38 +0800

    Implement Super

--- abc.js
+++ abc.js
@@ -5,18 +5,21 @@ var Class = require("./index.js");
 
 var A = Class({
     foo: function(a,b) {
+        console.log("A#foo",a,b)
         return [a,b];
     }
 });
 
 var B = Class({
     foo: function(a,b) {
+        console.log("B#foo",a,b)
         return this.super("foo",a*10,b*100);
     }
 },A);
 
 var C = Class({
     foo: function(a,b) {
+        console.log("C#foo",a,b)
         return this.super("foo",a*10,b*100);
     }
 },B);
